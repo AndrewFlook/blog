46a333948c60f8fab8c990fba96f27261b75971f
Andrew Flook <aflook88@gmail.com>
2020-04-05 09:54:48 -0700

    home.blade.php

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -4,25 +4,33 @@
 
     @if(Auth::check())
         <ul>
-            <li>Backend</li>
+            <a href="#"><li>Backend</li></a>
         </ul>
     @endif
 
-    @foreach($posts as $p)
-        <div class="max-w-sm w-full lg:max-w-full lg:flex">
-            <div class="border border-gray-400 bg-white rounded p-4 leading-normal shadow">
-                <div class="text-gray-900 font-bold text-xl mb-2">
-                    {{ $p->title }}
-                </div>
-                <div class="text-gray-700 text-base mb-4">
-                    {!! $p->body !!}
-                </div>
-                <div class="text-sm">
-                    <p class="text-gray-900 leading-none">{{ $p->author->username }}</p>
-                    <p class="text-gray-600">{{ $p->created_at->toDateTimeString() }}</p>
+    @if($posts >= 1)
+        @foreach($posts as $p)
+            <div class="max-w-sm w-full lg:max-w-full lg:flex">
+                <div class="border border-gray-400 bg-white rounded p-4 leading-normal shadow">
+                    <div class="text-gray-900 font-bold text-xl mb-2">
+                        {{ $p->title }}
+                    </div>
+                    <div class="text-gray-700 text-base mb-4">
+                        {!! $p->body !!}
+                    </div>
+                    <div class="text-sm">
+                        <p class="text-gray-900 leading-none">{{ $p->author->username }}</p>
+                        <p class="text-gray-600">{{ $p->created_at->toDateTimeString() }}</p>
+                    </div>
                 </div>
             </div>
+        @endforeach
+    @else
+        <div class="w-full border border-gray-400 bg-white rounded p-4 leading-normal shadow">
+            <p class="text-gray-700 text-base mb-4">
+                There's nothing here. Yet. Come back Soon™
+            </p>
         </div>
-    @endforeach
+    @endif
 
 @endsection
